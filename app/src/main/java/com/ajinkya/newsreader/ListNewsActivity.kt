8e865f8712be36e3d3847a9d5c3bd0108c149b2c
Ajinkya Kalaskar <ajinkya5130@gmail.com>
2020-02-17 21:40:24 +0530

    code done in Master by mistake

---
+++ app/src/main/java/com/ajinkya/newsreader/ListNewsActivity.kt
@@ -0,0 +1,12 @@
+package com.ajinkya.newsreader
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListNewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_news)
+    }
+}
--- app/src/main/java/com/ajinkya/newsreader/common/CommonCall.kt
+++ app/src/main/java/com/ajinkya/newsreader/common/CommonCall.kt
@@ -5,8 +5,20 @@ import com.ajinkya.newsreader.listeners.NewsService
 object CommonCall{
 
     private const val BASE_URL = "https://newsapi.org"
-    //val API_KEY = "beae63bb4dbd4283a81ba08067fb7d71"
+    val API_KEY = "beae63bb4dbd4283a81ba08067fb7d71"
+    var country = "in"
 
     val newsService:NewsService
     get() = RetrofitCall.getClient(BASE_URL).create(NewsService::class.java)
+
+
+    fun getNewsApi(source: String): String {
+        val apiUrl = StringBuilder("http://newsapi.org/v2/top-headlines?sources=")
+            .append(source)
+            .append("@apiKey=")
+            .append(API_KEY)
+            .toString()
+
+        return apiUrl
+    }
 }
